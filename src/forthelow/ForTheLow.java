d679144d36652e78e63759c542e7d4d476fac1f0
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-19 21:17:55 -0400

    Update ForTheLow.java

--- src/forthelow/ForTheLow.java
+++ src/forthelow/ForTheLow.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package forthelow;
 
-/**
- *
- * @author junio
- */
 public class ForTheLow {
-
-    /**
-     * @param args the command line arguments
-     */
+//main function
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
