dacd4a2525c23149f69c44d51d5bbc87c14206a8
junio <junio@192.168.2.223>
2022-04-19 21:26:15 -0400

    my Initial Commit

--- src/forthelow/ForTheLow.java
+++ src/forthelow/ForTheLow.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package forthelow;
 
-/**
- *
- * @author junio
- */
-public class ForTheLow {
 
-    /**
-     * @param args the command line arguments
-     */
+public class ForTheLow {
+//main class 
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
