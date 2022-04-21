c24c06d983377eeac73abc9211cf12267806b94e
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-21 10:39:45 -0400

    Update Posts.java

--- src/forthelow/Posts.java
+++ src/forthelow/Posts.java
@@ -1,18 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package forthelow;
 
-/**
- *
- * @author junio
- */
 public class Posts {
     
     Private Cars carToSell;
     
+    private String author;
     
+    public Posts(double price, String brand, Dealership dealer, String color, String author) {
+        super(price, brand, dealer, color);
+        this.author = author;
+    }
     
 }
