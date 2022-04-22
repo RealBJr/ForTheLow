2c51297cec7087c8847ac1d04f7db632ad04f1a7
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-22 15:46:32 -0400

    Update SearchBar.java

--- src/forthelow/SearchBar.java
+++ src/forthelow/SearchBar.java
@@ -6,7 +6,9 @@ public class SearchBar {
         for(int i = 0; i < search.length; i++){
             if(search.charAt(i) == 32){
                 String keyword = search.substring(0, i);
-                
+                for(j = 0; j<Inventory.getInventory.size(); i++){
+                    
+                }
             }
         }
     }
