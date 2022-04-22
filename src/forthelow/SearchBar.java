46fdffd1f177735f214f5f3859a520bbf710b7f9
Keethen <100371714+Keethen21@users.noreply.github.com>
2022-04-22 18:51:46 -0400

    Update SearchBar.java

--- src/forthelow/SearchBar.java
+++ src/forthelow/SearchBar.java
@@ -5,24 +5,56 @@ public class SearchBar {
    public Inventory search(String search){
         Inventory result = new Inventory();
         
+        
+        
         for(int i = 0; i < search.length(); i++){//basic implementation where we only search for first keyword
             if(search.charAt(i) == 32){
                 String keyword = search.substring(0, i);
                 Inventory inventory = new Inventory();
                 
                 for(int j = 0; j < inventory.getInventory().size(); j++){
-                    Collection<Post> suitableCars = inventory.getInventory().keySet();
-                    Iterator<Post> isuitableCars = suitableCars.iterator();
+                    Collection<Posts> suitableCars = inventory.getInventory().keySet();
+                    Iterator<Posts> isuitableCars = suitableCars.iterator();
                     
                     while(isuitableCars.hasNext()){
-                        if (isuitableCars.next().getKeywords().contains(keyword)) {
-                            result.getInventory().put(isuitableCars.next(), isuitableCars.next().getSetOfNbrs());
+                        Posts n = isuitableCars.next();
+                        if (n.getKeywords().contains(keyword)) {
+                            
+                            result.getInventory().clear();
+                            result.getInventory().put(n, n.getSetOfNbrs());
                         }
                     }
                 }
             }
+            
+            else {
+                
+               String keyword = search; 
+               Inventory inventory = new Inventory();
+               
+                 for(int j = 0; j < inventory.getInventory().size(); j++){
+                    Collection<Posts> suitableCars = inventory.getInventory().keySet();
+                    Iterator<Posts> isuitableCars = suitableCars.iterator();
+                    
+                    while(isuitableCars.hasNext()){
+                        Posts n = isuitableCars.next();
+                        if (n.getKeywords().contains(keyword)) {
+                            
+                            result.getInventory().clear();
+                            result.getInventory().put(n, n.getSetOfNbrs());
+                        }
+                    }
+                }
+               
+               
+                
+                
+                
+            }
+            
+            
+            
         }
         return result;
-    }
     
 }
