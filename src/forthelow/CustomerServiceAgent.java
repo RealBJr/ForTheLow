5316efffc18c0cb71752cb4b51945446e99d027f
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-22 16:31:59 -0400

    Update CustomerServiceAgent.java

--- src/forthelow/CustomerServiceAgent.java
+++ src/forthelow/CustomerServiceAgent.java
@@ -14,7 +14,7 @@ public class CustomerServiceAgent {
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("What is your income? \n Press 1: 25K - 50K annually \n Press 2: 50K -75K annually \n Press 3: 75K - 100K annually \n Press 1: 100K and above");
+        System.out.println("What is your income? \n Press 1: <25K \n Press 1: 25K - 50K annually \n Press 2: 50K -75K annually \n Press 3: 75K - 100K annually \n Press 4: 100K and above");
         String answer1 = sc.nextLine();
         
         System.out.println("Do you have a preferred car model? \n Press 1: minivan \n Press 2: sportscar \n Press 3: pick-up truck \n Press 4: luxury car \n Press 5: sedan");
