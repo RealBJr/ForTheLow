364a342b24311a72987b7d171ea0a753a34eefbd
Keethen <100371714+Keethen21@users.noreply.github.com>
2022-04-22 14:48:51 -0400

    Update ForTheLow.java

--- src/forthelow/ForTheLow.java
+++ src/forthelow/ForTheLow.java
@@ -6,6 +6,30 @@ public class ForTheLow {
 //main class 
     public static void main(String[] args) {
 
+        
+        do {
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        } while( optionEntered != 8);
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
