ae24f38855ca16112429ff6205f6f26549c401c1
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-22 16:12:10 -0400

    Update Cars.java

--- src/forthelow/Cars.java
+++ src/forthelow/Cars.java
@@ -8,10 +8,10 @@ abstract public class  Cars {
     private String color;
     private int lifestyle, salary, colorPreference = 0; 
     ptivate String model;
-    private String buyOrLease;
     
     
-    public Cars(double price, String brand, String model, String addressOfDealer, String color, String buyOrLease) {
+    
+    public Cars(double price, String brand, String model, String addressOfDealer, String color) {
         this.price = price;
         this.brand = brand;
         this.dealer = dealer;
