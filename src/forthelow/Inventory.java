1a33f61e7a22c6cc5675c8c4dcbb6754b6a86dec
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-22 16:02:18 -0400

    Update Inventory.java

--- src/forthelow/Inventory.java
+++ src/forthelow/Inventory.java
@@ -2,8 +2,17 @@
 package forthelow;
 
 public class Inventory {
-    private HashMap<Cars, String> inventory;
-    public void display(ArrayList<Posts> posts){
+    private HashMap <Cars, String> inventory = new HashMap<>();
+
+    public HashMap<Cars, String> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(HashMap<Cars, String> inventory) {
+        this.inventory = inventory;
+    }
+    public static void main(String[] args) {
+        inventory.get(args)
     }
    
     
