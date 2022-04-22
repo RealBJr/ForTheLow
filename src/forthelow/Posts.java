51ac266e529b4cb51ccd329411d821c627a13798
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-04-22 16:09:29 -0400

    Update Posts.java

--- src/forthelow/Posts.java
+++ src/forthelow/Posts.java
@@ -7,8 +7,8 @@ public class Post extends Cars{
     
     private String author;
     
-    public Post(double price, String brand, String model, Dealership dealer, String color, String author) {
-        super(price, brand, model, dealer, color);
+    public Post(double price, String brand, String model, Dealership dealer, String color, String author, String buyOrLease) {
+        super(price, brand, model, dealer, color, buyOrLease);
         this.author = author;
     }
     public void buy(){
