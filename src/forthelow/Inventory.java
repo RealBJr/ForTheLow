43b71cfa13dbdea56f0291ed9b0f77e20bae70a0
Keethen <100371714+Keethen21@users.noreply.github.com>
2022-05-01 00:06:15 -0400

    Update Inventory.java
    added a few cars

--- src/forthelow/Inventory.java
+++ src/forthelow/Inventory.java
@@ -46,6 +46,10 @@ public class Inventory {
         inventory.put( new Posts(51000.0, "Ford" , "F-150 2019 ", "7100 Rue Saint-Jacques, Montreal, QC H4B 1V2",  "white" , "100 451 km" ) , "3,3,4");
         inventory.put(new Posts(390000, "RollsRoyce" , " Wraith 2017" , "8525 Decarie Blvd, Montreal, Quebec H4P 2J2" , "black" , "4096 km"), "5,4,2");
         inventory.put(new Posts(55000, "Mitsubishi" , " Outlander 2022" , " 2465 Bd du Curé-Labelle, Laval, QC H7T 1R3" , "brown" , "35000 km"), "3,6,2");
+          inventory.put(new Posts(13590, "Kia", "Forte Ex 2014", "2250 Bd Crémazie O, Montréal, QC H2P 1C6", "brown", 101834 ), "1,5,4");
+        inventory.put(new Posts(59654, "Acura", "Rdx 2021", "4040 Rue Jean-Talon O, Montréal, QC H4P 1V5", "black", 11650 ), "3,6,2");
+         inventory.put(new Posts(169800, "Audi", "RS 7 2021", "5805 Trans Canada Route, Saint-Laurent, Quebec H4T 1A1", "Grey", 10 ), "5,2,1");
+
             
         }
               
