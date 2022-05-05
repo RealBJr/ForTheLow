b0d69c87b7f5b096cc4eeba48b019c17aec12089
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-05-05 11:11:17 -0400

    Update ForTheLow.java

--- src/forthelow/ForTheLow.java
+++ src/forthelow/ForTheLow.java
@@ -1,9 +1,3 @@
-
-package forthelow;
-
-import java.util.Scanner;
-
-
 package forthelow;
 
 import java.util.Comparator;
@@ -15,117 +9,74 @@ public class ForTheLow {
     public static void main(String[] args) {
         UserManagement userM = new UserManagement();
         Scanner sc = new Scanner(System.in);
-        
+
         int optionEntered = 0;
-        while(optionEntered != 5) {
-        try {
-        
-        // int optionEntered;
-        do {
-            
-             optionEntered = userM.menuTab();
-            
-            
-            if (optionEntered == 1) { 
-            
-            SearchBar search = new SearchBar();
-                System.out.println("What car u want?");
-                String ans1 = sc.nextLine();
-                System.out.println("How do you want to sort: 1 = Price wise, 2 = Mileage wise");
-                int ans2 = sc.nextInt();
-                    if (ans2 == 1) {
-                    
-                      //  TreeMap<Posts,String> trMap = new TreeMap<>(new Comparators1());
-                        
-                        
-                      
-                       Inventory inv = search.search(ans1, new Comparators1());
-                                 
-                                 
-                                inv.display();
-                                inv.displayInventoryPositionDecision(); 
-                                inv.Decision();
-                                         
-                                 
-                                 
-                                 
-                        
-                                 
-                        
+        while (optionEntered != 5) {
+            try {
+
+                // int optionEntered;
+                do {
+
+                    optionEntered = userM.menuTab();
+
+                    if (optionEntered == 1) {
+
+                        SearchBar search = new SearchBar();
+                        System.out.println("What car u want?");
+                        String ans1 = sc.nextLine();
+                        System.out.println("How do you want to sort: 1 = Price wise, 2 = Mileage wise");
+                        int ans2 = sc.nextInt();
+                        if (ans2 == 1) {
+
+                            //  TreeMap<Posts,String> trMap = new TreeMap<>(new Comparators1());
+                            Inventory inv = search.search(ans1, new Comparators1());
+
+                            inv.display();
+                            inv.displayInventoryPositionDecision();
+                            inv.Decision();
+
+                        } else if (ans2 == 2) {
+
+                            Inventory inv2 = search.search(ans1, new Comparators1());
+                            inv2.display();
+                            inv2.displayInventoryPositionDecision();
+                            inv2.Decision();
+
+                        }
+                        //TODO implement comparator
+
+                    } else if (optionEntered == 2) {
+
+                        CustomerServiceAgent csa = new CustomerServiceAgent();
+
+                        csa.getName();
+                        csa.questions();
+
+                    } else if (optionEntered == 3) {
+
+                    } else if (optionEntered == 4) {
+
+                        Inventory inv = new Inventory();
+                        inv.display();
+                        inv.displayInventoryPositionDecision();
+                        inv.Decision();
+
+                    } else if (optionEntered == 5) {
+
+                        System.out.println("See you next time :) ");
+
                     }
-                    else if (ans2 == 2) {
-                    
-                         Inventory inv2 = search.search(ans1, new Comparators1());
-                         inv2.display();
-                         inv2.displayInventoryPositionDecision();
-                         inv2.Decision();
-                                 
-                            
-                    }
-                //TODO implement comparator
-                
-                
-            
-            }
-            
-            
-             else if (optionEntered == 2) { 
-            
-            CustomerServiceAgent csa = new CustomerServiceAgent();
-           
-            csa.getName();
-            csa.questions();
-            
-            
-            }
-            
-            else  if (optionEntered == 3) { 
-            
-            
-            
-            
-            }
-            
-            
-             else  if (optionEntered == 4) { 
-            
-                 Inventory inv = new Inventory();
-                 inv.display();
-                 inv.displayInventoryPositionDecision(); 
-                 inv.Decision();
-                
-            
-            
-            }
-            
-             else if (optionEntered == 5) {
-                
-                  System.out.println("See you next time :) " );
-                  
+
+                } while (optionEntered != 5);
+
+            } catch (Exception e) {
+
+                System.out.println("Please try again");
 
             }
-                     
-            
-        } while( optionEntered != 5);
-        
-        
-        
-        
-        } catch(Exception e){
-            
-            System.out.println("Please try again");
-            
-            
+
         }
-        
-        
-        }
-        
+
     }
-        
-    
+
 }
-    
-
-
-
