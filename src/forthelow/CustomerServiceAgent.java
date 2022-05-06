4dbd835eb07a1bc553e3fedd8dfd3021e75584e2
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-05-06 15:34:36 -0400

    Update CustomerServiceAgent.java

--- src/forthelow/CustomerServiceAgent.java
+++ src/forthelow/CustomerServiceAgent.java
@@ -9,55 +9,55 @@ import java.util.Stack;
 import java.util.TreeMap;
 
 public class CustomerServiceAgent {
-
+    
     private String clientsFirstName;
     private String clientsLastName;
-
+    
     public String getClientsFirstName() {
         return clientsFirstName;
     }
-
+    
     public void setClientsFirstName(String clientsFirstName) {
         this.clientsFirstName = clientsFirstName;
     }
-
+    
     public String getClientsLastName() {
         return clientsLastName;
     }
-
+    
     public void setClientsLastName(String clientsLastName) {
         this.clientsLastName = clientsLastName;
     }
-
+    
     public String getName() {
-
+        
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.println("what is your first name");
         clientsFirstName = sc.nextLine();
-
+        
         System.out.println("What is your last name");
         clientsLastName = sc.nextLine();
-
+        
         return clientsFirstName + clientsLastName;
     }
-
+    
     public String questions() {
-
+        
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.println("What is your income? \n Press 1: <25K \n Press 2: 25K - 50K annually \n Press 3: 50K -75K annually \n Press 4: 75K - 100K annually \n Press 5: 100K and above");
         String answer1 = sc.nextLine();
-
+        
         System.out.println("Do you have a preferred car model? \n Press 1: minivan \n Press 2: sportscar \n Press 3: pick-up truck \n Press 4: luxury car \n Press 5: compact cars \n Press 6 : SUV");
         String answer2 = sc.nextLine();
-
+        
         System.out.println("What mileage? \n Press 1: brand new (no mileage) \n Press 2: 100km - 50k km \n Press 3: 50k-100k km \n Press 4: 100k+ km");
         String answer3 = sc.nextLine();
-
+        
         return "" + answer1 + answer2 + answer3;
     }
-
+    
     public Inventory displayCarPreferences(String answerList) {
         Inventory cars = new Inventory();
         final ArrayList<Character> charsAnswerList = new ArrayList<>();
@@ -73,28 +73,30 @@ public class CustomerServiceAgent {
         //creating a List of values that i would easily manipulate
         List<String> valuesAsList = new ArrayList();
         valuesAsList.addAll(values);
-
+        
         int maxRepeated = 0;
-        TreeMap<Post,String> recommendation = new TreeMap<>();
-
-        for (int i = 0; i < valuesAsList.size(); i++) {
-            String singleValue = valuesAsList.get(i);
-            int repeated = 0; // nbr of times there are common nbrs
-                for(int j = 0; j<singleValue.length(); j++){
-                if (singleValue.charAt(j) == charsAnswerList.get(j)) {
-                    repeated++;
+        TreeMap<Posts, String> recommendation = new TreeMap<>();
+        if (charsAnswerList.get(0).equals(valuesAsList.get(0).charAt(0))) {
+            for (int i = 1; i < valuesAsList.size(); i++) {
+                String singleValue = valuesAsList.get(i);
+                int repeated = 0; // nbr of times there are common nbrs
+                for (int j = 1; j < singleValue.length(); j++) {
+                    if (singleValue.charAt(j) == charsAnswerList.get(j)) {
+                        repeated++;
+                    }
                 }
-            }
-            
-            if (repeated > maxRepeated) {
-                maxRepeated = repeated;
-                recommendation.put(getKey(cars.getInventory(), valuesAsList.get(i)), valuesAsList.get(i));
-            }
-            if (repeated == maxRepeated) {
-                recommendation.put(getKey(cars.getInventory(), valuesAsList.get(i)), valuesAsList.get(i));
-            }
+                
+                if (repeated > maxRepeated) {
+                    maxRepeated = repeated;
+                    recommendation.put(getKey(cars.getInventory(), valuesAsList.get(i)), valuesAsList.get(i));
+                }
+                if (repeated == maxRepeated) {
+                    recommendation.put(getKey(cars.getInventory(), valuesAsList.get(i)), valuesAsList.get(i));
+                }
+            }            
         }
-        cars.setInventory(new TreeMap<Post, String>(recommendation));
+        
+        cars.setInventory(new TreeMap<Posts, String>(recommendation));
         return cars;
     }
     
@@ -106,9 +108,10 @@ public class CustomerServiceAgent {
         }
         return null;
     }
+
     public static void main(String[] args) {
         CustomerServiceAgent csa = new CustomerServiceAgent();
         csa.displayCarPreferences(csa.questions());
     }
-
+    
 }
