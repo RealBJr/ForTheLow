8de48ca09332cd69645031093dc8c1c3dce1dd2f
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-05-08 18:50:04 -0400

    Update SearchBar.java

--- src/forthelow/SearchBar.java
+++ src/forthelow/SearchBar.java
@@ -1,19 +1,17 @@
 package forthelow;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SearchBar {
 
-    public  Inventory search(String search, Comparator com){
+    public Inventory search(String search, Comparator com) {
         Inventory result = new Inventory(com);
-        
-       // LinkedHashSet<Posts> lhm = new LinkedHashSet<>();
-        
-        
-         for (int i = 0; i < search.length(); i++) {//basic implementation where we only search for first keyword
-             
-             if (search.charAt(i) == ' ') {
+
+        for (int i = 0; i < search.length(); i++) {
+
+            if (search.charAt(i) == ' ') {
                 String keyword = search.substring(0, i);
                 Inventory inventory = new Inventory();
 
@@ -22,24 +20,13 @@ public class SearchBar {
                         .filter(n -> n.getKeywords().contains(keyword))
                         .collect(Collectors.toList());
 
-                
                 for (int j = 0; j < collected.size(); j++) {
                     result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
                 }
-//                    for(int j = 0; j < inventory.getInventory().size(); j++){
-//                    Collection<Posts> suitableCars = inventory.getInventory().keySet();
-//                    Iterator<Posts> isuitableCars = suitableCars.iterator();
-//                    
-//                    while(isuitableCars.hasNext()){
-//                        Posts n = isuitableCars.next();
-//                        if (n.getKeywords().contains(keyword)) {
-//                            
-//                            result.getInventory().clear();
-//                            result.getInventory().put(n, n.getSetOfNbrs());
-//                        }
-//                    }
-//                }
-            } else {
+                search = search.substring(i + 1);
+                i = -1;
+
+            } else if (i == search.length() - 1) {
 
                 String keyword = search;
                 Inventory inventory = new Inventory();
@@ -49,37 +36,13 @@ public class SearchBar {
                         .filter(n -> n.getKeywords().contains(keyword))
                         .collect(Collectors.toList());
 
-               
                 for (int j = 0; j < collected.size(); j++) {
                     result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
                 }
-                
             }
-            search = search.substring(i+1);
         }
-       if (result.getInventory().isEmpty()){
-            String keyword = search.strip().stripTrailing().toLowerCase();
-            Inventory inventory = new Inventory();
 
-                List<Posts> collected = inventory.getInventory().keySet()
-                        .stream()
-                        .filter(n -> n.getKeywords().contains(keyword))
-                        .collect(Collectors.toList());
-                
-                for (int j = 0; j < collected.size(); j++) {
-                    result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
-                }
-        }
-        return result ;
-    }
-    
-   
-     public static void main(String[] args) {
-        SearchBar s = new SearchBar();
-        s.search("white", new Comparators1()).display();
-        
+        return result;
     }
 
 }
-
-
