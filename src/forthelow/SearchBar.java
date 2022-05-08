0e5e2e778eab0f61d3fb537e4b0646b848cf2a97
Keethen <100371714+Keethen21@users.noreply.github.com>
2022-05-08 00:12:42 -0400

    Update SearchBar.java

--- src/forthelow/SearchBar.java
+++ src/forthelow/SearchBar.java
@@ -5,10 +5,13 @@ import java.util.stream.Collectors;
 
 public class SearchBar {
 
-    public Inventory search(String search) {
-        Inventory result = new Inventory();
-
-        for (int i = 0; i < search.length(); i++) {//basic implementation where we only search for first keyword
+    public  Inventory search(String search, Comparator com){
+        Inventory result = new Inventory(com);
+        
+       // LinkedHashSet<Posts> lhm = new LinkedHashSet<>();
+        
+        
+         for (int i = 0; i < search.length(); i++) {//basic implementation where we only search for first keyword
             if (search.charAt(i) == ' ') {
                 String keyword = search.substring(0, i);
                 Inventory inventory = new Inventory();
@@ -18,7 +21,7 @@ public class SearchBar {
                         .filter(n -> n.getKeywords().contains(keyword))
                         .collect(Collectors.toList());
 
-                result.getInventory().clear();
+                
                 for (int j = 0; j < collected.size(); j++) {
                     result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
                 }
@@ -45,20 +48,22 @@ public class SearchBar {
                         .filter(n -> n.getKeywords().contains(keyword))
                         .collect(Collectors.toList());
 
-                result.getInventory().clear();
+               
                 for (int j = 0; j < collected.size(); j++) {
                     result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
                 }
                 
             }
-
+            search = search.substring(i+1);
         }
         return result ;
     }
     
-    public static void main(String[] args) {
+   
+     public static void main(String[] args) {
         SearchBar s = new SearchBar();
-        s.search("white");
+        s.search("white", new Comparators1()).display();
+        
     }
 
 }
