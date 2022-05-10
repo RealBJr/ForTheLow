16d109951106df5d89a05f947767f30174234a28
Keethen <100371714+Keethen21@users.noreply.github.com>
2022-05-10 16:28:40 -0400

    Update Posts.java

--- src/forthelow/Posts.java
+++ src/forthelow/Posts.java
@@ -8,22 +8,49 @@ package javaapplication1;
 
 /**
  *
- * @author keethen
+ * @author 2177095
  */
-public class Posted extends Posts{
-    
-    public Posted(double price, String brand, String model, String addressOfDealer, String phoneNumber, String color, int mileage) {
-        super(price, brand, model, addressOfDealer, phoneNumber, color, mileage);
-    }
-    
-    String type  = "Posted";
+public class Posts extends Cars implements Comparable<Posts> {
+
+     private Cars carToSell;
+
+    private int inventoryPosition;
+    private int position = 0;
+    String type = "Posts";
 
     public String getType() {
-        return this.type;
+        return type;
     }
-    
-    
-    
-    
-}
 
+    public int getInventoryPosition() {
+        return inventoryPosition;
+    }
+
+    public void setInventoryPosition(int inventoryPosition) {
+        this.inventoryPosition = inventoryPosition;
+    }
+
+    private String author;
+
+    public Posts(double price, String brand, String model, String addressOfDealer, String phoneNumber, String color, int mileage) {
+        super(price, brand, model, addressOfDealer, phoneNumber, color, mileage);
+        this.carToSell = carToSell;
+        this.author = author;
+
+    }
+
+    public void buy() {
+    }
+
+    public void bookAnAppointment() {
+    }
+
+    @Override
+    public int compareTo(Posts o) {
+
+        int value = this.getBrand().compareTo(o.getBrand());
+
+        return value;
+
+    }
+}
