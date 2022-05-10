663b1f1a678de2faad5b9242566b9facdbfe9ce2
RealBJr <102128249+RealBJr@users.noreply.github.com>
2022-05-09 21:33:25 -0400

    Update SearchBar.java

--- src/forthelow/SearchBar.java
+++ src/forthelow/SearchBar.java
@@ -1,7 +1,10 @@
 package forthelow;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class SearchBar {
@@ -21,7 +24,7 @@ public class SearchBar {
                         .collect(Collectors.toList());
 
                 for (int j = 0; j < collected.size(); j++) {
-                    result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
+                    result.getInventoryCopy().put(collected.get(j), collected.get(j).getSetOfNbrs());
                 }
                 search = search.substring(i + 1);
                 i = -1;
@@ -37,12 +40,30 @@ public class SearchBar {
                         .collect(Collectors.toList());
 
                 for (int j = 0; j < collected.size(); j++) {
-                    result.getInventory().put(collected.get(j), collected.get(j).getSetOfNbrs());
+                    result.getInventoryCopy().put(collected.get(j), collected.get(j).getSetOfNbrs());
                 }
             }
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        Inventory i = new Inventory();
+        i.display();
+        i.posted(new Posts(76888.00, "Maserati", "Levante Q4S 2018",
+                "  8525 Decarie Blvd, Mount Royal, Quebec H4P 2J2", "(514) 738-3030", "red", 1500), "442");
+        
+        
+               
+        System.out.println("");
+        
+        i.display();
+        
+        System.out.println("");
+        
+        SearchBar s = new SearchBar();
+        s.search("red", new Comparators1()).display();
     }
 
 }
